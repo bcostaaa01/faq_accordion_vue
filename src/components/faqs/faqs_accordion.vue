a828b5b0fdcfdb647a4e9aecde12d133f88bd64d
Bruno <14274839-bcostaaa01@users.noreply.gitlab.com>
2023-12-21 15:32:45 +0000

    refactor(accordion): refactor icon path

--- src/components/faqs/faqs_accordion.vue
+++ src/components/faqs/faqs_accordion.vue
@@ -7,7 +7,7 @@ import Card from '@/components/card/Card.vue'
     <Card
       description="Read our most frequently asked questions."
       heading="FAQs"
-      icon="./../src/assets/images/icon-star.svg"
+      icon="../../../assets/images/icon-star.svg"
     />
   </span>
 </template>
